7634b58a0cf89227bc8e1526527f7381221deaa1
Josiah <sircazadorr@gmail.com>
2025-09-01 10:15:23 -0400

    Add project files.

---
+++ Chess Logic/Direction.cs
@@ -0,0 +1,34 @@
+﻿namespace Chess_Logic
+{
+    //Class for directions pieces can go
+    public class Direction
+    {
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction UpLeft = Up + Left;
+        public readonly static Direction UpRight = Up + Right;
+        public readonly static Direction DownLeft = Down + Left;
+        public readonly static Direction DownRight = Down + Right;
+
+        public int RowDelta {  get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(dir1.RowDelta + dir2.RowDelta, dir1.ColumnDelta + dir2.ColumnDelta);
+        }
+
+        public static Direction operator *(Direction dir, int scalar)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+    }
+}
---
+++ Chess UI/Images.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Chess_Logic;
+
+namespace Chess_UI
+{
+    public static class Images
+    {
+        private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+        {
+            { PieceType.Pawn, LoadImage("Assets/PawnW.png") },
+            { PieceType.Rook, LoadImage("Assets/RookW.png") },
+            { PieceType.Knight, LoadImage("Assets/knightW.png") },
+            { PieceType.Bishop, LoadImage("Assets/BishopW.png") },
+            { PieceType.Queen, LoadImage("Assets/QueenW.png") },
+            { PieceType.King, LoadImage("Assets/KingW.png") }
+        };
+
+        private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+        {
+            { PieceType.Pawn, LoadImage("Assets/PawnB.png") },
+            { PieceType.Rook, LoadImage("Assets/RookB.png") },
+            { PieceType.Knight, LoadImage("Assets/knightB.png") },
+            { PieceType.Bishop, LoadImage("Assets/BishopB.png") },
+            { PieceType.Queen, LoadImage("Assets/QueenB.png") },
+            { PieceType.King, LoadImage("Assets/KingB.png") }
+        };
+        private static ImageSource LoadImage(string filepath)
+        {
+            return new BitmapImage(new Uri(filepath, UriKind.Relative));
+        }
+
+        public static ImageSource GetImage(Player color, PieceType type)
+        {
+            return color switch
+            {
+                Player.White => whiteSources[type],
+                Player.Black => blackSources[type],
+                _ => null
+            };
+        }
+        public static ImageSource GetImage(Piece piece)
+        {
+            if (piece == null)
+            {
+                return null;
+            }
+
+            return GetImage(piece.Color, piece.Type);
+        }
+
+    }
+}
